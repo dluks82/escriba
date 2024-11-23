96ae9a8c63c7d1902b34d6fa56df18f9c4c99ae3
Diogo Oliveira <dluks82@gmail.com>
2024-11-23 09:22:07 -0300

    :wrench: chore: Start Project Configs

---
+++ src/main/java/dev/dluks/escriba/EscribaApplication.java
@@ -0,0 +1,13 @@
+package dev.dluks.escriba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscribaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscribaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dluks/escriba/config/TestConfig.java
@@ -0,0 +1,10 @@
+package dev.dluks.escriba.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+
+@TestConfiguration
+@ActiveProfiles("test")
+public class TestConfig {
+    // Configurações específicas para testes podem ser adicionadas aqui
+}
