4be565eeac0b142d9ef716af2ab3e01696973fee
Diogo Oliveira <dluks82@gmail.com>
2024-11-24 16:19:40 -0300

    :sparkles: feat: Add `AtribuicaoService` and unit tests

---
+++ src/main/java/dev/dluks/escriba/services/AtribuicaoService.java
@@ -0,0 +1,114 @@
+package dev.dluks.escriba.services;
+
+import dev.dluks.escriba.domain.entities.Atribuicao;
+import dev.dluks.escriba.domain.exceptions.DuplicateResourceException;
+import dev.dluks.escriba.domain.exceptions.ResourceNotFoundException;
+import dev.dluks.escriba.domain.repositories.AtribuicaoRepository;
+import dev.dluks.escriba.dtos.atribuicao.AtribuicaoResponse;
+import dev.dluks.escriba.dtos.atribuicao.AtribuicaoResponseMin;
+import dev.dluks.escriba.dtos.atribuicao.CreateAtribuicaoRequest;
+import dev.dluks.escriba.dtos.atribuicao.UpdateAtribuicaoRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AtribuicaoService {
+
+    private final AtribuicaoRepository repository;
+
+    @Transactional
+    public AtribuicaoResponse create(CreateAtribuicaoRequest dto) {
+        validateDuplicatedId(dto.getId());
+        validateDuplicateNome(dto.getNome());
+
+        Atribuicao atribuicao = repository.save(
+                Atribuicao.builder()
+                        .id(dto.getId())
+                        .nome(dto.getNome())
+                        .situacao(dto.getSituacao() != null ? dto.getSituacao() : true)
+                        .build());
+
+        return AtribuicaoResponse.fromEntity(atribuicao);
+    }
+
+    @Transactional
+    public AtribuicaoResponse update(String id, UpdateAtribuicaoRequest dto) {
+        Atribuicao atribuicao = findOrFail(id);
+
+        if (!atribuicao.getNome().equalsIgnoreCase(dto.getNome())) {
+            validateDuplicateNome(dto.getNome());
+        }
+
+        Atribuicao atribuicaoAtualizada = repository.save(Atribuicao.builder()
+                .id(id)
+                .nome(dto.getNome())
+                .build());
+
+        return AtribuicaoResponse.fromEntity(atribuicaoAtualizada);
+    }
+
+    public Page<AtribuicaoResponseMin> findAll(Pageable pageable) {
+        return repository.findAll(pageable)
+                .map(AtribuicaoResponseMin::fromEntity);
+    }
+
+    public List<AtribuicaoResponseMin> findAvailable() {
+        return repository.findBySituacaoTrue()
+                .stream()
+                .map(AtribuicaoResponseMin::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public AtribuicaoResponse findById(String id) {
+        return AtribuicaoResponse.fromEntity(findOrFail(id));
+    }
+
+    @Transactional
+    public AtribuicaoResponse changeSituacao(String id, Boolean situacao) {
+        Atribuicao atribuicao = findOrFail(id);
+
+        if (situacao) {
+            atribuicao.ativar();
+        } else {
+            atribuicao.desativar();
+        }
+
+        return AtribuicaoResponse.fromEntity(repository.save(atribuicao));
+    }
+
+    @Transactional
+    public void delete(String id) {
+        Atribuicao atribuicao = findOrFail(id);
+        repository.delete(atribuicao);
+    }
+
+    private Atribuicao findOrFail(String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Atribuição não encontrada: " + id));
+    }
+
+    private void validateDuplicatedId(String id) {
+        if (repository.existsById(id)) {
+            throw new DuplicateResourceException("Registro já cadastrado");
+        }
+    }
+
+    private void validateDuplicateNome(String nome) {
+        Optional<Atribuicao> atribuicao = repository.findByNomeIgnoreCase(nome);
+
+        if (atribuicao.isPresent()) {
+            throw new DuplicateResourceException(
+                    String.format("Nome já informado no registro com código %s", atribuicao.get().getId())
+            );
+        }
+    }
+
+}
---
+++ src/test/java/dev/dluks/escriba/services/AtribuicaoServiceTest.java
@@ -0,0 +1,218 @@
+package dev.dluks.escriba.services;
+
+import dev.dluks.escriba.domain.entities.Atribuicao;
+import dev.dluks.escriba.domain.exceptions.DuplicateResourceException;
+import dev.dluks.escriba.domain.exceptions.ResourceNotFoundException;
+import dev.dluks.escriba.domain.repositories.AtribuicaoRepository;
+import dev.dluks.escriba.dtos.atribuicao.AtribuicaoResponse;
+import dev.dluks.escriba.dtos.atribuicao.AtribuicaoResponseMin;
+import dev.dluks.escriba.dtos.atribuicao.CreateAtribuicaoRequest;
+import dev.dluks.escriba.dtos.atribuicao.UpdateAtribuicaoRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class AtribuicaoServiceTest {
+
+    @Mock
+    private AtribuicaoRepository repository;
+
+    @InjectMocks
+    private AtribuicaoService service;
+
+    private CreateAtribuicaoRequest dtoCreate;
+    private CreateAtribuicaoRequest dtoCreateWithSituacaoFalse;
+    private UpdateAtribuicaoRequest dtoUpdate;
+    private UpdateAtribuicaoRequest dtoUpdateSame;
+    private Atribuicao atribuicao;
+    private Atribuicao atribuicaoWithSituacaoFalse;
+
+    @BeforeEach
+    void setup() {
+        dtoCreate = CreateAtribuicaoRequest.builder()
+                .id("ATRIB_TEST")
+                .nome("Test")
+                .build();
+
+        dtoCreateWithSituacaoFalse = CreateAtribuicaoRequest.builder()
+                .id("ATRIB_TEST")
+                .nome("Test")
+                .situacao(false)
+                .build();
+
+        dtoUpdate = UpdateAtribuicaoRequest.builder()
+                .nome("Teste")
+                .build();
+
+        dtoUpdateSame = UpdateAtribuicaoRequest.builder()
+                .nome("Test")
+                .build();
+
+        atribuicao = Atribuicao.builder()
+                .id("ATRIB_TEST")
+                .nome("Test")
+                .situacao(true)
+                .build();
+
+        atribuicaoWithSituacaoFalse = Atribuicao.builder()
+                .id("ATRIB_TEST")
+                .nome("Test")
+                .situacao(false)
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("Deve criar uma atribuição")
+    void shouldCreateAtribuicao() {
+        when(repository.existsById(dtoCreate.getId())).thenReturn(false);
+        when(repository.findByNomeIgnoreCase(dtoCreate.getNome())).thenReturn(Optional.empty());
+        when(repository.save(any(Atribuicao.class))).thenReturn(atribuicao);
+
+        AtribuicaoResponse result = service.create(dtoCreate);
+
+        assertNotNull(result);
+        assertEquals(dtoCreate.getId(), result.getId());
+        assertEquals(dtoCreate.getNome(), result.getNome());
+        verify(repository).save(any(Atribuicao.class));
+    }
+
+    @Test
+    @DisplayName("Deve criar uma atribuição com situação false")
+    void shouldCreateAtribuicaoWithSituacaoFalse() {
+        when(repository.existsById(dtoCreateWithSituacaoFalse.getId())).thenReturn(false);
+        when(repository.findByNomeIgnoreCase(dtoCreateWithSituacaoFalse.getNome())).thenReturn(Optional.empty());
+        when(repository.save(any(Atribuicao.class))).thenReturn(atribuicaoWithSituacaoFalse);
+
+        AtribuicaoResponse result = service.create(dtoCreateWithSituacaoFalse);
+
+        assertNotNull(result);
+        assertEquals(dtoCreateWithSituacaoFalse.getId(), result.getId());
+        assertEquals(dtoCreateWithSituacaoFalse.getNome(), result.getNome());
+        assertFalse(result.isSituacao());
+        verify(repository).save(any(Atribuicao.class));
+    }
+
+    @Test
+    @DisplayName("Não deve criar uma atribuição com id duplicado")
+    void shouldNotCreateAtribuicaoWithDuplicatedId() {
+        when(repository.existsById(dtoCreate.getId())).thenReturn(true);
+
+        assertThrows(DuplicateResourceException.class, () -> service.create(dtoCreate));
+    }
+
+    @Test
+    @DisplayName("Não deve criar uma atribuição com nome duplicado")
+    void shouldNotCreateAtribuicaoWithDuplicatedName() {
+        when(repository.existsById(dtoCreate.getId())).thenReturn(false);
+        when(repository.findByNomeIgnoreCase(dtoCreate.getNome())).thenReturn(Optional.of(atribuicao));
+        when(repository.existsById(dtoCreate.getId())).thenReturn(false);
+
+        when(repository.findByNomeIgnoreCase(dtoCreate.getNome()))
+                .thenReturn(Optional.of(atribuicao));
+
+        assertThrows(DuplicateResourceException.class, () -> service
+                .create(dtoCreate));
+    }
+
+    @Test
+    @DisplayName("Deve falhar ao buscar situação por id")
+    void shouldFailToFindAtribuicaoById() {
+        String id = "ATRIB_TEST";
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> service.update(id, dtoUpdate));
+    }
+
+    @Test
+    @DisplayName("Deve listar atribuições com situação true")
+    void shouldListAtribuicoesWithSituacaoTrue() {
+        when(repository.findBySituacaoTrue()).thenReturn(List.of(atribuicao));
+
+        List<AtribuicaoResponseMin> resultado = service.findAvailable();
+
+        assertFalse(resultado.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Deve ativar uma atribuição")
+    void shouldActivateAtribuicao() {
+        String id = "ATRIB_TESTE";
+
+        when(repository.findById(id)).thenReturn(Optional.of(atribuicao));
+        when(repository.save(any())).thenAnswer(i -> i.getArgument(0));
+
+        AtribuicaoResponse resultado = service.changeSituacao(id, true);
+
+        assertTrue(resultado.isSituacao());
+    }
+
+    @Test
+    @DisplayName("Deve desativar uma atribuição")
+    void shouldDeactivateAtribuicao() {
+        String id = "ATRIB_TESTE";
+
+        when(repository.findById(id)).thenReturn(Optional.of(atribuicao));
+        when(repository.save(any())).thenAnswer(i -> i.getArgument(0));
+
+        AtribuicaoResponse resultado = service.changeSituacao(id, false);
+
+        assertFalse(resultado.isSituacao());
+    }
+
+    @Test
+    @DisplayName("Deve atualizar atribuição")
+    void shouldUpdateAtribuicao() {
+        when(repository.findById(dtoCreate.getId())).thenReturn(Optional.of(atribuicao));
+        when(repository.findByNomeIgnoreCase(dtoCreate.getNome())).thenReturn(Optional.empty());
+        when(repository.save(any(Atribuicao.class))).thenReturn(atribuicao);
+
+        AtribuicaoResponse result = service.update(dtoCreate.getId(), dtoUpdate);
+
+        assertNotNull(result);
+        assertEquals(dtoCreate.getNome(), result.getNome());
+        verify(repository).save(any(Atribuicao.class));
+    }
+
+    @Test
+    @DisplayName("Deve atualizar atribuição com nome igual ao original")
+    void shouldUpdateAtribuicaoWithSameName() {
+        when(repository.findById(dtoCreate.getId())).thenReturn(Optional.of(atribuicao));
+        when(repository.save(any(Atribuicao.class))).thenReturn(atribuicao);
+
+        AtribuicaoResponse result = service.update(dtoCreate.getId(), dtoUpdateSame);
+
+        assertNotNull(result);
+        assertEquals(dtoCreate.getNome(), result.getNome());
+        verify(repository).save(any(Atribuicao.class));
+    }
+
+    @Test
+    @DisplayName("Deve apagar uma atribuição")
+    void shouldDeleteAtribuicao() {
+        String id = "ATRIB_TESTE";
+
+        when(repository.findById(id)).thenReturn(Optional.of(atribuicao));
+
+        service.delete(id);
+
+        verify(repository).delete(atribuicao);
+    }
+
+}
