786252730ff01097d7341b69f94b4f507bae1d42
Diogo Oliveira <dluks82@gmail.com>
2024-11-25 10:20:31 -0300

    :sparkles: feat: Add `IntegrityConstraintException` class

---
+++ src/main/java/dev/dluks/escriba/domain/exceptions/IntegrityConstraintException.java
@@ -0,0 +1,9 @@
+package dev.dluks.escriba.domain.exceptions;
+
+public class IntegrityConstraintException extends EscribaException {
+
+    public IntegrityConstraintException(String message) {
+        super(message);
+    }
+
+}
